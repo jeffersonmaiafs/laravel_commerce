3a273fb8815c31f908332dd9281b24e9206f4c53
jeffersonmaiafs <jeffersonmaia.si@gmail.com>
2017-05-13 17:55:39 -0300

    Atividade sobre rotas
    +  +  + PROFESSOR TIVE UMA DÚVIDA + + + +
    *      Quando utilizo o name route(coloco um apelido com o 'as') tive
    dúvidas
    *      porque quando utilizo o parâmetro
    Route::get('categoriagerais/{id?}'
    *      No exemplo da aula não vi nenhum teste com parametros e o "as"
    *      na hora de criar a rota coloca no as com ou sem parâmetros,
    exemplo:
    *      assim: 'categoriagerais/{id?}',['as'=>'categorias',...
    *
    *   ou assim: 'categoriagerais/{id?}',['as'=>'categorias/{id?}',...
    *
    *       OBS:  Favor me reportar caso seja encontrado algum erro.*
    *  */

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,95 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+/* * ******    RESPONDENDO A QUESTÃO DO MÓDULO 3 SOBRE ROTAS * * * * * * * * */
+//
+/* Criação das rotas
+  Agora que você já possui os dois models criados, crie as rotas necessárias para que possamos realizar um CRUD em cada model.
+  Para facilitar a administração do arquivo de rotas, as mesmas deverão ser totalmente agrupadas pelo prefixo: admin e pelo seu próprio model.
+  Exemplo: admin/products, admin/categories
+  OBS: Enquanto nem todos os controllers e actions ainda não estão definidos, aponte as rotas para um controller e action qualquer.
+  OBS 2: Todas as rotas devem possuir nome e seus parâmetros devem ser validados.
+ */
+
+/*
+ *          +  +  + PROFESSOR TIVE UMA DÚVIDA + + + + 
+ *      Quando utilizo o name route(coloco um apelido com o 'as') tive dúvidas
+ *      porque quando utilizo o parâmetro Route::get('categoriagerais/{id?}'
+ *      No exemplo da aula não vi nenhum teste com parametros e o "as"
+ *      na hora de criar a rota coloca no as com ou sem parâmetros, exemplo:
+ *      assim: 'categoriagerais/{id?}',['as'=>'categorias',...
+ * 
+ *   ou assim: 'categoriagerais/{id?}',['as'=>'categorias/{id?}',...
+ *      
+ *       OBS: FAVOR ME REPORTAR OS ERROS PARA QUE EU APRENDA, OBRIGADO.
+ * 
+ *  */
+
+// INICIO
+
+Route::pattern('id', '[0-9]+');
+Route::group(['prefix' =>'admin'], function(){
+    Route::get('categoriagerais/{id?}',['as'=>'categorias', function($id = null) {
+        if ($id) {
+            $category = new \Portfolio\Category();
+            $c = $category->find($id);
+            echo("<br/><br/>");
+            return $c->name;
+        } else {
+            return "<br/>Não possui ID.<br/>";
+        }
+    }]);
+   echo(route('categorias'));
+   echo("<br/><br/>");
+    
+    Route::get('produtosgerais/{id?}',['as'=>'produtos',function($id = null) {
+        if ($id) {
+            $products = new \Portfolio\Products();
+            $p = $products->find($id);
+            echo("<br/><br/>");
+            return $p->name;
+        } else {
+            return "<br/>Não possui ID.<br/>";
+        }
+    }]);
+   echo("<br/><br/>"); 
+   echo(route('produtos'));
+
+  
 });
+
+//FIM
+
+//Route::get('categorias', 'AdminCategoriesController@consulta');
+
+//Testando este no momento
+Route::get('exemplo', 'WelcomeController@exemplo');
+
+Route::get('/', 'WelcomeController@index');
+
+//Direciona para o index através do caminho localhost:8000/admin/categories
+Route::get('admin/categories', 'AdminCategoriesController@index');
+
+//Direciona para o index através do caminho localhost:8000/admin/categories
+Route::get('admin/products', 'AdminProductsController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
+
+
+
+
